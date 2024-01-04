1ac5b1cf46a406a83eaac1f30762a7d74e8cd262
Your Name <amitakhileshtiwari@gmail.com>
2024-01-04 20:55:49 +0530

    routes

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const tourRouter = express.Router();
 const fs = require('fs');
 const tour = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours.json`));
+
 const getAllTours=(req,res)=>{
     console.log("localhost");
     res.status(200).json({
@@ -10,6 +11,14 @@ const getAllTours=(req,res)=>{
         tour
     })
 }
+const getTours=(req,res)=>{
+    console.log("EK ek karke denge tours");
+    console.log();
+    res.status(200).json({
+        status:'success'
+    })
+}
 tourRouter.route('/').get(getAllTours);
+tourRouter.route('/:id').get(getTours);
 
 module.exports = tourRouter;
