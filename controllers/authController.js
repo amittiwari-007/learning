bef9fea4369e46cc152eaa52898e18e8a40fdfc7
Your Name <amitakhileshtiwari@gmail.com>
2024-04-09 17:01:07 +0530

    Update till error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const catchAsync = require('./../utils/catchAsync')
 
 exports.signup = catchAsync(async (req,res,next)=>{
     const newUser = await User.create(req.body);
-
+    console.log("Check");
     res.status(200).json({
         status:'success',
         data:{
